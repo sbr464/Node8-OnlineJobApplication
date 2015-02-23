7706b2af269caa2203d3a4de18659fe81990e918
Stephen Richardson <sbr464@gmail.com>
2015-02-23 11:45:31 -0700

    updates

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Omega');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+
+
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -20,7 +24,8 @@ app.get('/applicants', function(req, res){
 app.post('/applicant', function(req, res){
   // Here is where you need to get the data
   // from the post body and store it in the database
-  res.send('No funciona');
+  console.log(req.body);
+  res.render('success');
 });
 
 var server = app.listen(8441, function() {
